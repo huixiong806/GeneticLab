c88c098b03d4bf13e0cc6f13dd4446a5e6619326
orange_bird <qiaoyuanfeilong@163.com>
2016-12-29 00:38:17 +0800

    补充更新 supplement update
    now version: ALPHA 1.5.2

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -4,10 +4,7 @@
 #include "Floop.h"
 #include "Sloop.h"
 #include "publicVars.h"
-//#include <windows.h>
-#include <sstream>
 USING_NS_CC;
-using namespace std;
 Scene* World::createScene()
 {
     // 'scene' is an autorelease object
@@ -38,19 +35,22 @@ bool World::init()
 	//初始化食物
 	for (int i = 0; i < 700*16; i++)
 	{
-		std::shared_ptr<Food> newFood = make_shared<Food>(*this, 1);
+		std::shared_ptr<Food> newFood = std::make_shared<Food>(*this, 1);
 		food.insert(newFood);
 		newFood->addToChunk(newFood->getChunk(*this), newFood);
 	}
+	bloopCount[(int)BloopType::gloop] = 0;
+	bloopCount[(int)BloopType::floop] = 0;
+	bloopCount[(int)BloopType::sloop] = 0;
 	//初始化bloops
-	for (int i = 0; i < 60*16; i++)
+	for (int i = 0; i < 90 * 16; i++)
 	{
-		if(i<2*16)
-			bloop.emplace(make_shared<Sloop>(*this, 1));
-		else if (i<11*16)
-			bloop.emplace(make_shared<Floop>(*this, 1));
+		if (i < 2 * 16)
+			addBloop(std::make_shared<Sloop>(*this, 1));
+		else if (i<16 * 16)
+			addBloop(std::make_shared<Floop>(*this, 1));
 		else 
-			bloop.emplace(make_shared<Gloop>(*this, 1));
+			addBloop(std::make_shared<Gloop>(*this, 1));
 	}
 	camera = Vec2(0, 0);
 	for (int i = 0; i <= 3; i++)
@@ -65,55 +65,36 @@ bool World::init()
 	backGround->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(backGround, 0);
 	info = "KeyControl\nD:Zoom In\nF:Zoom Out\nX:Hide/Show information\nVersion1.5\ncopyright 2016 orangebird.\nPublished with MIT licence.";
-	information = Label::createWithTTF(info,"fonts/Marker Felt.ttf", 75);
+	information = Label::createWithTTF(info,"fonts/Marker Felt.ttf", 60);
 	information->setColor(Color3B(104,209,255));
-	information->setPosition(origin + Vec2(visibleSize.width / 4.0*3.0, visibleSize.height / 4.0));
+	information->setPosition(origin + Vec2(visibleSize.width / 5.0*4.0, visibleSize.height / 4.0));
 	showInformation = true;
 	this->addChild(information,2);
-	statCD = 0;
+	recorder = Recorder();
+	recorder.setFrequency(500);
 	tick = 0;
-	SYSTEMTIME sysTime;
-	ZeroMemory(&sysTime, sizeof(sysTime));
-	GetLocalTime(&sysTime);
-	stringstream filePath;
-	filePath << "data\\experiment" << (int)sysTime.wYear << "." << (int)sysTime.wMonth << "." << (int)sysTime.wDay<<".";
-	filePath << (int)sysTime.wHour << "_" << (int)sysTime.wMinute << "_" << (int)sysTime.wSecond << ".csv";
-	dataOutPut.open(filePath.str().c_str(), ios::out);
-	dataOutPut << "tick,food,Gloop,Floop,Sloop" << endl;
 	schedule(schedule_selector(World::eventProcessor), 0.02f);
     return true;
 }
-//数据统计输出,20s进行一次
+//数据统计输出
 void World::outputData()
 {
-	int GloopCount = 0, FloopCount = 0, SloopCount = 0;
-	for (auto &i : bloop)
+	if (bloopCount[(int)BloopType::floop] < 160)
+		for (int i = 0; i < 100; i++)
+			addBloop(std::make_shared<Floop>(*this, 1));
+	if (bloopCount[(int)BloopType::sloop] < 10)
+		for (int i = 0; i < 10; i++)
+			addBloop(std::make_shared<Sloop>(*this, 1));
+	recorder.tick(std::vector<unsigned long long>
 	{
-		if (i->die)continue;
-		switch (i->bloopType)
-		{
-		case BloopType::gloop:
-			++GloopCount;
-			break;
-		case BloopType::floop:
-			++FloopCount;
-			break;
-		case BloopType::sloop:
-			++SloopCount;
-			break;
-		}
-	}
-	if (statCD == 0)
-	{
-		statCD = 499;
-		dataOutPut << tick << "," << food.size() << ",";
-		dataOutPut << GloopCount << "," << FloopCount << "," << SloopCount << endl;
-	}
-	else statCD--;
-	stringstream str;
-	str << "Tick:" << tick << "\nGloop:" << GloopCount << " Floop:" << FloopCount << " Sloop:" << SloopCount << '\n' << info;
+		food.size(),
+		bloopCount[(int)BloopType::gloop],
+		bloopCount[(int)BloopType::floop],
+		bloopCount[(int)BloopType::sloop],
+	});
+	std::stringstream str;
+	str << "Tick:" << tick << "\nFood:"<< food.size() << "\nGloop:" << bloopCount[(int)BloopType::gloop] << " Floop:" << bloopCount[(int)BloopType::floop] << " Sloop:" << bloopCount[(int)BloopType::sloop] << '\n' << info;
 	information->setString(str.str());
-	++tick;
 }
 void World::setChunkCount(Size chunkCount)
 {
@@ -124,11 +105,26 @@ void World::setChunkCount(Size chunkCount)
 	for (auto &i : chunk)
 		i.resize(chunkCount.height+1);
 }
+void World::addBloop(std::shared_ptr<Bloop> bloop)
+{
+	this->bloop.insert(bloop);
+	bloop->addToChunk(bloop->getChunk(*this));
+	++bloopCount[(int)bloop->bloopType];
+}
+std::unordered_set<std::shared_ptr<Bloop>>::const_iterator 
+	World::eraseBloop(std::unordered_set<std::shared_ptr<Bloop>>::const_iterator target)
+{
+	(*target)->removeFromChunk((*target)->getChunk(*this));
+	--bloopCount[(int)(*target)->bloopType];
+	return bloop.erase(target);
+}
 void World::eventProcessor(float dt)
 {
 	auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
 	const Vec2 deltaPos[4] = { Vec2(0,1),Vec2(0,-1), Vec2(-1,0), Vec2(1,0) };
+	//增加tick
+	++tick;
 	//移动视野
 	for (int i = 0; i <= 3; i++)
 		if (keyGroupCamera[i])camera += deltaPos[i]*(int)(glview->getDesignResolutionSize().width/204.8*1.2);
@@ -136,15 +132,12 @@ void World::eventProcessor(float dt)
 	for (auto iter = bloop.begin(); iter != bloop.end();)
 	{
 		if ((*iter)->die)
-		{
-			(*iter)->removeFromChunk((*iter)->getChunk(*this),*iter);
-			iter = bloop.erase(iter);
-		}
+			iter = eraseBloop(iter);
 		else ++iter;
 	}
 	//每个bloop执行tick动作
 	for (auto &item : bloop)
-		item->tick(*this,item);
+		item->tick(*this);
 	//绘制食物
 	for (auto &i : food)
 		i->refreshPosition(camera);
@@ -192,9 +185,8 @@ void World::onKeyPressed(EventKeyboard::KeyCode keyCode, Event* event)
 		curResolutionSize = glview->getDesignResolutionSize();
 		backGround->setScale(curResolutionSize.width/backGround->getContentSize().width);
 		information->setScale(curResolutionSize.width / backGround->getContentSize().width);
-		information->setPosition(Vec2(curResolutionSize.width / 4.0*3.0, curResolutionSize.height / 4.0));
+		information->setPosition(Vec2(curResolutionSize.width / 5.0*4.0, curResolutionSize.height / 4.0));
 		backGround->setPosition(curResolutionSize / 2);
-		
 	}
 	else if (keyCode == EventKeyboard::KeyCode::KEY_D)  //D键，减小screensize
 	{
@@ -203,7 +195,7 @@ void World::onKeyPressed(EventKeyboard::KeyCode keyCode, Event* event)
 		curResolutionSize = glview->getDesignResolutionSize();
 		backGround->setScale(curResolutionSize.width / backGround->getContentSize().width);
 		information->setScale(curResolutionSize.width / backGround->getContentSize().width);
-		information->setPosition(Vec2(curResolutionSize.width / 4.0*3.0, curResolutionSize.height / 4.0));
+		information->setPosition(Vec2(curResolutionSize.width / 5.0*4.0, curResolutionSize.height / 4.0));
 		backGround->setPosition(curResolutionSize / 2);
 	}
 	else if (keyCode == EventKeyboard::KeyCode::KEY_X)
